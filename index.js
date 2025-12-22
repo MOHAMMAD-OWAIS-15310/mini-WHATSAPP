d8bb62bf43e36a88d0f5a2510f91a4f2f3a78f8d
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2025-12-22 23:35:27 +0530

    added create new chat , edit message and date

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const Chat = require("./models/chats.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 
 app.get("/",(req,res)=>{
@@ -24,6 +25,41 @@ app.get("/chats",async(req,res)=>{
     // res.send("working");
     res.render("index.ejs",{ chats });
 })
+
+//new route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+//create route
+app.post("/chats",(req,res)=>{
+    let {from,to,message}=req.body;
+    let newChat=new Chat({
+        from: from,
+        to : to ,
+        message :message,
+        created_at :Date(),
+    });
+    // console.log(newChat);
+    newChat.save()
+        .then((res)=>{
+            console.log("chat saved ");
+        })
+        .catch((err)=>{
+            console.log("error occured here!!!!");
+        });
+
+    res.redirect("/chats");
+})
+
+//edit route
+app.get("/chats/:id/edit",async (req,res)=>{
+    // res.send("edit working");
+    let {id}=req.params;
+    let chat= await Chat.findById(id); //await bc we dont use then here , so put sync upar
+    res.render("edit.ejs",{chat});
+})
+
 
 // let chat1=new Chat({
 //     from: "owais",
