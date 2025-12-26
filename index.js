731c64652f28d2f238a60d604511c023c4d80c53
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2025-12-26 12:12:35 +0530

    added try catch for all async and await

--- index.js
+++ index.js
@@ -22,52 +22,94 @@ app.listen(3000,(req,res)=>{
 
 //index route
 app.get("/chats",async(req,res)=>{
-    let chats=await Chat.find();
+    try{
+        let chats=await Chat.find();
     console.log(chats);
     // res.send("working");
     res.render("index.ejs",{ chats });
+    }
+    catch(err){
+        res.status(500).send("error!!");
+    }
+    
 })
 
 //new route
 app.get("/chats/new",(req,res)=>{
-    res.render("new.ejs");
+        res.render("new.ejs");
+
 })
 
 //create route
-app.post("/chats",(req,res)=>{
-    let {from,to,message}=req.body;
-    let newChat=new Chat({
-        from: from,
-        to : to ,
-        message :message,
-        created_at :Date(),
-    });
-    // console.log(newChat);
-    newChat.save()
-        .then((res)=>{
-            console.log("chat saved ");
-        })
-        .catch((err)=>{
-            console.log("error occured here!!!!");
+app.post("/chats",async (req,res)=>{
+    try{   // for aync we use try catch to handle error
+        let {from,to,message}=req.body;
+        if (!from || (!to) || !message) {
+                return res.status(400).send("error !! all fields are required");
+        }
+        let newChat=new Chat({
+            from: from,
+            to : to ,
+            message :message,
+            created_at :Date(),
         });
+        // console.log(newChat);
+        newChat.save()
+            .then((res)=>{
+                console.log("chat saved ");
+            })
+            .catch((err)=>{
+                res.status(400).send("error occured");
+            });
 
-    res.redirect("/chats");
+        res.redirect("/chats");
+    }
+    catch(err){
+         res.status(500).send("Error in saving ");
+    }
 })
 
 //edit route
 app.get("/chats/:id/edit",async (req,res)=>{
-    // res.send("edit working");
-    let {id}=req.params;
-    let chat= await Chat.findById(id); //await bc we dont use then here , so put sync upar
-    res.render("edit.ejs",{chat});
+    try{
+            // res.send("edit working");
+        let {id}=req.params;
+        let chat= await Chat.findById(id); //await bc we dont use then here , so put sync upar
+        res.render("edit.ejs",{chat});
+
+    }
+    catch(err){
+        res.status(400).send("error!!! invalid id");
+    }
+
+    
 })
 
 //update route
 app.put("/chats/:id",async(req,res)=>{
-    let {id}=req.params;
-    let {message : newMessage}=req.body;
-    let updatedChat=await Chat.findByIdAndUpdate(id , {message : newMessage},{runValidators:true, new : true});
-    res.redirect("/chats");
+    try{
+
+        let {id}=req.params;
+        let {message : newMessage}=req.body;
+        let updatedChat=await Chat.findByIdAndUpdate(id , {message : newMessage},{runValidators:true, new : true});
+        res.redirect("/chats");
+    }
+    catch(err){
+        res.status(400).send("error!!! ");
+    }
+})
+
+//delete route
+app.delete("/chats/:id",async (req,res)=>{
+    try{
+        let {id}=req.params;
+        let deletedChat =await Chat.findByIdAndDelete(id);
+        res.redirect("/chats");
+    }
+    catch(err){
+        res.status(400).send("error!!! ");
+    }
+    
 })
 
 
